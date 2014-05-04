aee52572ff650c07ffaa81b6d3daf2d3c6efe239
Matt Foster <matt@fostermatt.com>
2014-05-04 01:15:09 -0700

    mostly working, need to finish percDown, increaseKey, and removeElement

--- MattFoster1.c
+++ MattFoster1.c
@@ -9,12 +9,12 @@ Assignment 6
 #include <stdbool.h>
 
 int deleteHeap(int*);
-void buildHeap();
+int buildHeap(int*,int,char*);
 int insertElement(int*, int, int);
 void deleteMin();
 void findMax();
 void increaseKey();
-void decreaseKey();
+void decreaseKey(int*,int,int);
 void removeElement();
 void printHeap(int*,int);
 void percUp(int*,int);
@@ -24,7 +24,7 @@ int main(){
 	char* prompt1 = "Please choose an option\n(1) Delete Heap\n(2) Build Heap\n(3) Insert Element\n";
 	char* prompt2 = "(4) Delete Minimum Element\n(5) Find Maximum Element\n(6) Increase Key\n";
 	char* prompt3 = "(7) Decrease Key\n(8) Remove Element\n(9) Exit\n\nOption: ";
-	int currIndex = 1; /*current empty element*/
+	int nextIndex = 1; /*current empty element*/
 	int userSelect, temp1, temp2,i;
 	char inBuff[200];
 	int heap[33];
@@ -40,20 +40,25 @@ int main(){
 		switch(userSelect){
 			case 1: /*Delete Heap*/
 				printf("\nHeap deleted\n");
-				currIndex = deleteHeap(heap);
+				nextIndex = deleteHeap(heap);
 				break;
 			case 2: /*Build Heap*/
-				printf("\nPlease enter numbers: ");
+				printf("\nPlease enter up to 32 numbers: ");
+				fgets(inBuff,sizeof(inBuff),stdin);
+				nextIndex = deleteHeap(heap);
+				nextIndex = buildHeap(heap,nextIndex,inBuff);
 				break;
 			case 3: /*Insert Element*/
 				printf("\nPlease enter value to be inserted: ");
 				fgets(inBuff,sizeof(inBuff),stdin);
 				sscanf(inBuff,"%d",&temp1);
-				currIndex = insertElement(heap,currIndex,temp1);
+				nextIndex = insertElement(heap,nextIndex,temp1);
 				break;
 			case 4: /*Delete Minimum Element*/
+				deleteMin(heap,nextIndex);
 				break;
 			case 5: /*Find Maximum Element*/
+				findMax(heap,nextIndex);
 				break;
 			case 6: /*Increase Key Value*/
 				printf("\nPlease enter index of element to increase: ");
@@ -61,7 +66,12 @@ int main(){
 				break;
 			case 7: /*Decrease Key Value*/
 				printf("\nPlease enter index of element to decrease: ");
+				fgets(inBuff,sizeof(inBuff),stdin);
+				sscanf(inBuff,"%d",&temp1);
 				printf("\nPlease enter amount to decrease element: ");
+				fgets(inBuff,sizeof(inBuff),stdin);
+				sscanf(inBuff,"%d",&temp2);
+				decreaseKey(heap,temp1,temp2);
 				break;
 			case 8: /*Remove element*/
 				printf("\nPlease enter index of element to remove: ");
@@ -73,7 +83,7 @@ int main(){
 				printf("Please choose a valid option\n");
 		}
 		printf("\n");
-		printHeap(heap,currIndex);
+		printHeap(heap,nextIndex);
 	}
 }
 int deleteHeap(int heap[]){
@@ -83,105 +93,125 @@ int deleteHeap(int heap[]){
 	}
 	return 0;
 }
-void buildHeap(){
-
+int buildHeap(int heap[], int nextIndex, char inBuff[]){
+	int i, temp;
+	char *myToken;
+	myToken = strtok(inBuff," ");
+	heap[nextIndex]=atoi(myToken);
+	nextIndex++;
+	while(myToken){
+		temp = atoi(myToken);
+		heap[nextIndex] = temp;
+		nextIndex++;
+		myToken = strtok(NULL," ");
+	}
+	/*
+	percDown(heap,index);
+	*/
+	return nextIndex;
 }
 
-int insertElement(int heap[], int currIndex, int value){
-	heap[currIndex] = value;
-	percUp(heap,currIndex);
-	currIndex = currIndex + 1;
-	return currIndex;
+int insertElement(int heap[], int nextIndex, int value){
+	heap[nextIndex] = value;
+	if (heap[nextIndex]<heap[nextIndex/2] && nextIndex>0){
+		percUp(heap,nextIndex);
+	}
+	nextIndex = nextIndex + 1;
+	return nextIndex;
 }
 
-void deleteMin(int heap[], int currIndex){
-	int child1 = heap[1];
-	int child2 = heap[2];
-	/*Place last element at top!*/
-	heap[0] = heap[index];
-	/*Percolate number to next possible spot*/
-	percolateDown(heap,0,index);
+void deleteMin(int heap[], int nextIndex){
+	printf("The minimum value of the heap was %d\n", heap[0]);
+	heap[0]=heap[nextIndex-1];
 }
-void findMax(){
-
+void findMax(int heap[], int nextIndex){
+	int i, maxVal = heap[nextIndex/2];
+	for(i=nextIndex/2+1;i<nextIndex;i++){
+		if(heap[i]>maxVal){
+			maxVal=heap[i];
+		}
+	}
+	printf("\nThe maximum value of this heap is %d\n", maxVal);
 }
 void increaseKey(){
 
 }
-void decreaseKey(){
 
+void decreaseKey(int heap[],int index,int value){
+	heap[index] = heap[index]-value;
+	if (heap[index]<heap[index/2] && index>0){
+		percUp(heap,index);
+	}
 }
+
 void removeElement(){
 
 }
 
-void printHeap(int heap[],int currIndex){
-	int i=1,j,k=1;
-	while(i<5 && k<currIndex){
+void printHeap(int heap[],int nextIndex){
+	int i,j,k=0;
+	if(!nextIndex){
+		return;
+	}
+	while(k<32){
 		printf("Values:  ");
-		for(j=1;j<9;j++){
-			printf("%6d", heap[i*j]);
-			k++;
-			if(i*j==currIndex-1){
-				j=10;
-			}
+		for(i=1; i<9 && i+k < nextIndex; i++){
+			printf("%6d",heap[i+k]);
 		}
-		printf("\n");
-		printf("Indicies:");
-		for(j=1;j<=k;j++){
-			printf("%6d", i*j);
-			if((i*j)==k-1){
-				break;
-			}
+		printf("\nIndicies:");
+		for(i=1; i<9 && i+k <nextIndex; i++){
+			printf("%6d",i+k);
 		}
 		printf("\n\n");
-		i++;
-	}
-}
-void percUp(int* heap, int child){
-	int parent=1;
-	while(parent){
-		parent = (child-1)/2;
-		if(heap[parent] > heap[child]){
-			heap[parent] ^= heap[child];
-			heap[child] ^= heap[parent];
-			heap[parent] ^= heap[child];
+		if(i+k>=nextIndex){
+			k=100;
 		}
-		child = parent;
+		k+=8;
+	}
+	printf("\n\n");
+}
+
+void percUp(int heap[], int index){
+	/* will only be called if parent and child are different */
+	/* xor swap, update current index, call again if needed */
+	heap[index/2] ^= heap[index];
+	heap[index] ^= heap[index/2];
+	heap[index/2] ^= heap[index];
+	index = index/2;
+	if (heap[index]<heap[index/2] && index>0){
+		percUp(heap,index);
 	}
 }
 
 void percDown(){
-	int swapChild;
+	/*int swapChild;
 	int child1;
 	int child2;
 	while(1){
 		child1 = 2*parent+1;
 		child2 = 2*(parent+1);
 		swapChild = child1;
-		/*If childrens out of bounds, we have reached the end*/
+
 		if(child1 >= index && child2 >=index)
 			return;
-		/*If child2 is out of boundaries*/
+
 		if(child2 == index)
 			break;
 		if(heap[child2] < heap[child1])
 			swapChild = child2;
-		/*Swap appropriate child*/
+
 		if(heap[parent] > heap[swapChild]){
 			heap[parent] ^= heap[swapChild];
 			heap[swapChild] ^= heap[parent];
 			heap[parent] ^= heap[swapChild];
 		}
 		parent = swapChild;
-
 	}
-	/*Special case where only 1 child to compare*/
 	if(heap[parent] > heap[child1]){
 		heap[parent] ^= heap[child1];
 		heap[child1] ^= heap[parent];
 		heap[parent] ^= heap[child1];
-	}
+	}*/
 }
 
 /*
@@ -202,10 +232,10 @@ prompt for user input
 	compare leaves, return largest value
 (6) Increase Key
 	prompt for index and amount to change
-	percolate if needed
+	percolate down if needed
 (7) Decrease Key
 	prompt for index and amount to change
-	percolate if needed
+	percolate up if needed
 (8) Remove element
 	prompt for index
 	swap with min child, remove once it is a leaf
